739d2c73df4b1ef7d69f0443a4473f6cbabc5f97
Stefano Berri <sberri@illumina.com>
2024-03-28 13:40:48 +0000

    Tidy up

--- roachcase/_infrastructure/dynamodb_repositories.py
+++ roachcase/_infrastructure/dynamodb_repositories.py
@@ -13,7 +13,7 @@ import datetime
 @dataclasses.dataclass
 class Column:
     name: str
-    ctype: str  # will be a controlled dictionary
+    ctype: str  # one of "S"
     is_primary: bool = False
     is_indexed: bool = False
 
@@ -24,31 +24,31 @@ class NoTableError(ValueError):
 
 class DBGateway(abc.ABC):
     """Interface for a generic datatabase gateway where storage happens in
-    'tables' with 'columns'"""
+    'tables' with 'columns'. Agnostic of the domain model."""
 
     @abc.abstractmethod
     def list_tables(self) -> List[str]:
-        pass
+        """List names of available tables"""
 
     @abc.abstractmethod
     def remove_table(self, table_name: str) -> None:
-        pass
+        """Remove a table by name"""
 
     @abc.abstractmethod
-    def create_table(self, specs: Dict[str, Any]) -> None:
-        pass
+    def create_table(self, name: str, columns: List[Column]) -> None:
+        """Create a table by name and column specifications"""
 
     @abc.abstractmethod
     def add(self, table_name: str, item: Dict[str, Any]) -> None:
-        pass
+        """Add an item to `table_name`"""
 
     @abc.abstractmethod
     def get(self, table_name: str) -> Iterable[Dict[str, Any]]:
-        pass
+        """Get items from `table_name`"""
 
     @abc.abstractmethod
     def delete(self, table_name: str, item: Dict[str, Any]) -> None:
-        """Delete an interm from a table"""
+        """Delete an intem from a table"""
 
 
 class DynamoDBGateway(DBGateway):
@@ -60,16 +60,24 @@ class DynamoDBGateway(DBGateway):
     def __init__(self) -> None:
         self.__client = boto3.client("dynamodb")
 
-    def __build_table_specs(self, table_name: str) -> Dict[str, Any]:
+    def __build_table_specs(
+        self, table_name: str, columns: List[Column]
+    ) -> Dict[str, Any]:
+
+        keyschema = [
+            {"AttributeName": column.name, "KeyType": "HASH"}
+            for column in columns
+            if column.is_primary
+        ]
+        attribute_definition = [
+            {"AttributeName": column.name, "AttributeType": column.ctype}
+            for column in columns
+        ]
         result = {
             "TableName": table_name,
             "TableClass": "STANDARD",
-            "KeySchema": [
-                {"AttributeName": "name", "KeyType": "HASH"},
-            ],
-            "AttributeDefinitions": [
-                {"AttributeName": "name", "AttributeType": "S"},
-            ],
+            "KeySchema": keyschema,
+            "AttributeDefinitions": attribute_definition,
             "ProvisionedThroughput": {"ReadCapacityUnits": 5, "WriteCapacityUnits": 5},
         }
         return result
@@ -89,7 +97,7 @@ class DynamoDBGateway(DBGateway):
 
     def create_table(self, name: str, columns: List[Column]) -> None:
         """Create a table if it does not exist already"""
-        template = copy.deepcopy(self.__build_table_specs(name))
+        template = copy.deepcopy(self.__build_table_specs(name, columns))
         template["TableName"] = name
         if name not in self.list_tables():
             response = self.__client.create_table(**template)
@@ -127,52 +135,52 @@ class DynamoDBGateway(DBGateway):
 
 
 class InMemoryDBGateway(DBGateway):
-    """An implementation of DynamoDBGateway which does not actually connect to
-    the AWS service"""
+    """An implementation of DBGateway which does not actually connect to
+    real persistence, but stores entries in memories"""
 
     def __init__(self) -> None:
-        self.__client: Dict[str, Any] = {}
+        self.__store: Dict[str, Any] = {}
 
     def list_tables(self) -> List[str]:
-        return list(self.__client.keys())
+        return list(self.__store.keys())
 
     def remove_table(self, table_name: str) -> None:
-        if table_name in self.__client:
-            del self.__client[table_name]
+        if table_name in self.__store:
+            del self.__store[table_name]
 
     def create_table(self, name: str, columns: List[Column]) -> None:
-        if name not in self.__client:
-            self.__client[name] = []
+        if name not in self.__store:
+            self.__store[name] = []
 
     def add(self, table_name: str, item: Dict[str, Any]) -> None:
         """Add an item to a table"""
         self.__raise_if_no_table(table_name)
-        if item not in self.__client[table_name]:
-            self.__client[table_name].append(item)
+        if item not in self.__store[table_name]:
+            self.__store[table_name].append(item)
 
     def get(self, table_name: str) -> Iterable[Dict[str, Any]]:
         """Get all items from a table"""
         self.__raise_if_no_table(table_name)
-        for item in self.__client[table_name]:
+        for item in self.__store[table_name]:
             yield item
 
     def delete(self, table_name: str, item: Dict[str, Any]) -> None:
         """Delete an interm from a table"""
         self.__raise_if_no_table(table_name)
-        self.__client[table_name].remove(item)
+        self.__store[table_name].remove(item)
 
     def __raise_if_no_table(self, table_name: str) -> None:
-        if table_name not in self.__client:
+        if table_name not in self.__store:
             raise NoTableError()
 
 
-class DynamoDBPlayerRepository(_repositories.PlayerRepository):
-    """A conversion layers from the domain layer to the DynamoDB persistence"""
+class DBPlayerRepository(_repositories.PlayerRepository):
+    """A conversion layers from the domain layer to the database persistence"""
 
     __columns = [Column(name="name", ctype="S", is_primary=True)]
 
-    def __init__(self, dynamodb: DynamoDBGateway) -> None:
-        self.__dynamodb = dynamodb
+    def __init__(self, db_gateway: DBGateway) -> None:
+        self.__db_gateway = db_gateway
         self.__table_name = "players"
 
     def add(self, player: _entities.Player) -> None:
@@ -182,12 +190,17 @@ class DynamoDBPlayerRepository(_repositories.PlayerRepository):
             raise _repositories.PlayerAlreadyExistError()
         else:
             item = {"name": {"S": player.get_name()}}
-            self.__dynamodb.add(self.get_table_name(), item)
+            try:
+                self.__db_gateway.add(self.get_table_name(), item)
+            except NoTableError:
+                self.__db_gateway.create_table(self.__table_name, self.__columns)
+                self.__db_gateway.add(self.get_table_name(), item)
 
     def get(self) -> Iterable[_entities.Player]:
-        for item in self.__dynamodb.get(self.get_table_name()):
-            result = _entities.Player(item["name"]["S"])
-            yield result
+        if self.get_table_name() in self.__db_gateway.list_tables():
+            for item in self.__db_gateway.get(self.get_table_name()):
+                result = _entities.Player(item["name"]["S"])
+                yield result
 
     def get_table_name(self) -> str:
         return self.__table_name
--- tests/infrastructure/test_dynamodb_repositories.py
+++ tests/infrastructure/test_dynamodb_repositories.py
@@ -8,87 +8,91 @@ import botocore
 
 
 @pytest.fixture
-def gateway():
+def in_memory_gateway():
+    result = dynamodb_repositories.InMemoryDBGateway()
+    return result
+
+
+@pytest.fixture
+def dynamodb_gateway():
     result = dynamodb_repositories.DynamoDBGateway()
     return result
 
 
-@pytest.fixture
-def player_repo(gateway):
-    result = dynamodb_repositories.DynamoDBPlayerRepository(gateway)
-    result.set_table_name("__testing_players")
-    return result
+class TestInMemoryDBGateway:
+    @pytest.fixture
+    def gateway(self, in_memory_gateway):
+        return in_memory_gateway
 
-
-@pytest.fixture
-def empty_player_repository(gateway):
-    # we want to start from an empty table
-    table_name = "__testing_players"
-    for item in gateway.get(table_name):
-        gateway.delete(table_name=table_name, item=item)
-    result = dynamodb_repositories.DynamoDBPlayerRepository(gateway)
-    result.set_table_name(table_name)
-    return result
-
-
-class TestDynamoDBGateway:
-    @pytest.mark.livedb
     def test_list_tables(self, gateway):
         observed = gateway.list_tables()
         # We don't have expectations on # the tables available
         assert isinstance(observed, list)
 
-    @pytest.mark.slow
-    @pytest.mark.livedb
-    def test_add_get_item_on_empty_table(self, gateway, player_repo):
+    def test_add_get_item_on_empty_table(self, gateway):
         table_name = "__a_table_to_delete__"
-        player_repo.set_table_name(table_name)
         gateway.remove_table(table_name)
         item = {"name": {"S": "Player1"}}
         # No table, we raise an error
         with pytest.raises(dynamodb_repositories.NoTableError):
             response = gateway.add(table_name, item)
 
-        columns = [dynamodb_repositories.Column(name="name", ctype="S", is_primary=True)]
+        columns = [
+            dynamodb_repositories.Column(name="name", ctype="S", is_primary=True)
+        ]
         gateway.create_table(table_name, columns=columns)
 
-        # gateway.create_table(player_repo.get_table_specs())
         # no error raised now
         gateway.add(table_name, item)
         assert table_name in gateway.list_tables()
         observed = gateway.get(table_name)
         assert list(observed) == [item]
+        gateway.delete(table_name, item)
+        observed = gateway.get(table_name)
+        assert list(observed) == []
+
+        # cleanup
+        gateway.remove_table(table_name)
 
 
-class TestInMemoryDBGateway(TestDynamoDBGateway):
+
+class TestDynamoDBGateway(TestInMemoryDBGateway):
     @pytest.fixture
-    def gateway(self):
-        result = dynamodb_repositories.InMemoryDBGateway()
-        return result
+    def gateway(self, dynamodb_gateway):
+        return dynamodb_gateway
 
+    @pytest.mark.livedb
     def test_list_tables(self, gateway):
         super().test_list_tables(gateway)
 
-    def test_add_get_item_on_empty_table(self, gateway, player_repo):
-        super().test_add_get_item_on_empty_table(gateway, player_repo)
+    @pytest.mark.slow
+    @pytest.mark.livedb
+    def test_add_get_item_on_empty_table(self, gateway):
+        super().test_add_get_item_on_empty_table(gateway)
 
 
 class TestDynamoDBPlayerRepository(test_repositories.TestInMemoryPlayerRepository):
+    @pytest.fixture
+    def player_repo(self, in_memory_gateway):
+        result = dynamodb_repositories.DBPlayerRepository(in_memory_gateway)
+        table_name = "__testing_players"
+        result.set_table_name(table_name)
+        return result
 
     def test_set_get_table_name(self, player_repo):
         player_repo.set_table_name("__another_testing_players")
         assert player_repo.get_table_name() == "__another_testing_players"
 
-    def test_get_add_roundtrip(self, empty_player_repository, john):
-        empty_player_repository.add(john)
+    def test_get_add_roundtrip(self, player_repo, john):
+        player_repo.add(john)
         expected = [john]
-        observed = empty_player_repository.get()
+        observed = player_repo.get()
         assert list(observed) == expected
 
     @pytest.mark.livedb
-    def test_round_trip(self, john, empty_player_repository):
-        super().test_round_trip(john, empty_player_repository)
+    def test_round_trip(self, john, player_repo):
+        super().test_round_trip(john, player_repo)
 
     @pytest.mark.livedb
-    def test_adding_player_with_same_name_raises(self, john, empty_player_repository):
-        super().test_adding_player_with_same_name_raises(john, empty_player_repository)
+    def test_adding_player_with_same_name_raises(self, john, player_repo):
+        super().test_adding_player_with_same_name_raises(john, player_repo)
